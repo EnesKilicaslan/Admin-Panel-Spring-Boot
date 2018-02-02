8975ffa52ed1563c7e6807cfe6570a86d3dec047
eneskilicaslan <eeneskilicaslan@gmail.com>
2018-02-02 17:21:57 +0200

    add toString representation of entity class

--- src/main/java/com/kilicaslan/enes/entity/User.java
+++ src/main/java/com/kilicaslan/enes/entity/User.java
@@ -1,12 +1,33 @@
 package com.kilicaslan.enes.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	
+	@Column(name = "firstname")
 	private String firstName;
+	
+	@Column(name = "lastname")
 	private String lastName;
+	
+	@Column(name = "username")
 	private String userName;
+	
+	@Column(name = "password")
 	private String password;
+	
+	@Column(name = "role")
 	private String role;   
 	
 	public User() {
@@ -14,7 +35,6 @@ public class User {
 	}
 	
 	public User(int id, String firstName, String lastName, String userName, String password, String role) {
-		super();
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -70,6 +90,11 @@ public class User {
 	public void setRole(String role) {
 		this.role = role;
 	}
-	
 
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", userName=" + userName
+				+ ", password=" + password + ", role=" + role + "]";
+	}
+	
 }
