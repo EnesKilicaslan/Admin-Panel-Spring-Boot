e7f24b59b56f96875d30302e7caf9b13d8741cf3
eneskilicaslan <eeneskilicaslan@gmail.com>
2018-02-02 20:31:48 +0200

    add next and prev buttons for paging

--- src/main/resources/static/js/datatable.js
+++ src/main/resources/static/js/datatable.js
@@ -2,27 +2,20 @@
 $(document).ready( function () {
 	
 	var page=1;
+	updateTable(page);
 	
-	$.ajax( {
-		type: 'get',
-		url : window.location + "users/?page=3",
-		contentType: "application/json",
-		dataType: "json"
-	    
-	}).done(function(data) {
-		$.each(data, function(index, element) {
-			$('#usersTable tfoot').append('<tr><td>' + element.id + '</td><td>' + element.firstName + '</td><td>' + element.lastName
-					+ '</td><td>' + element.userName + '</td><td>' + element.password + '</td><td>' + element.role + '</tr>');
-		});
+	$("#nextPage").click(function(){
+		page = page +1;
+		updateTable(page);
 	});
 	
-	
-	
-	
-	
-	
-	
-	
+	$("#prevPage").click(function(){
+		
+		if(page > 1 ){
+			page = page -1;
+			updateTable(page);
+		}
+	});
 	
 	$( "#add-user" ).submit(function(event) {
 			$.ajax( {
@@ -38,9 +31,25 @@ $(document).ready( function () {
 			    })
 		 });		
 	})
-		
-		
 	
-
+	
 });
+
+function updateTable(p){
+	$('#usersTable tfoot').empty();
+	
+	$.ajax( {
+		type: 'get',
+		url : window.location + "users/?page="+p,
+		contentType: "application/json",
+		dataType: "json"
+	    
+	}).done(function(data) {
+		$.each(data, function(index, element) {
+			$('#usersTable tfoot').append('<tr><td>' + element.id + '</td><td>' + element.firstName + '</td><td>' + element.lastName
+					+ '</td><td>' + element.userName + '</td><td>' + element.password + '</td><td>' + element.role + '</tr>');
+		});
+	});
+	
+}
 
